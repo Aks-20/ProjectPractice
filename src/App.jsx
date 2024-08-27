5453104f5374fec511397fc9372a86fd9cead6af
Akshat Gupta <akshatgupta0043@gmail.com>
2024-08-27 17:58:40 +0530

    Added Banner and NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import CoinTable from './Components/CoinTable/CoinTable'
+import NavBar from './Components/NavBar/NavBar'
+import Banner from './Components/Banner/Banner'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      hello
+   
+    <NavBar/>
+    <Banner/>
+     <CoinTable/>
+      
     </>
   )
 }
---
+++ src/Components/CoinTable/CoinTable.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+function CoinTable(){
+   // useEffect(callback,)
+    return (
+        <>
+        
+         
+        
+        </>
+    )
+}
+export default CoinTable
